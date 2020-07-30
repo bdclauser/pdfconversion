0822a976f6aff8881dbadac1e7e6d08802f08e69
bdclauser <pzljug@hotmail.com>
2020-07-30 00:26:30 -0400

    pdf convert to png

---
+++ pdf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# pdf.py
+
+
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import(
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+images = convert_from_path('myfile.pdf')  # rename pdf file to execute
+for i, image in enumerate(images):
+    fname = 'image' + str(i) + '.png'
+    image.save(fname, "PNG")
