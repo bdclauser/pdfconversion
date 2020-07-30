545d2f2d29df7ed48aae8d5e45f6274576946645
bdclauser <pzljug@hotmail.com>
2020-07-30 00:27:31 -0400

    resize images

--- pdf.py
+++ pdf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # pdf.py
 
+from image_resize import *
 
 from pdf2image import convert_from_path, convert_from_bytes
 from pdf2image.exceptions import(
@@ -13,3 +14,5 @@ images = convert_from_path('myfile.pdf')  # rename pdf file to execute
 for i, image in enumerate(images):
     fname = 'image' + str(i) + '.png'
     image.save(fname, "PNG")
+
+newsize()
