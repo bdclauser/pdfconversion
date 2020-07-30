035086092177612012fcb35709ee8aa01a752b42
bdclauser <pzljug@hotmail.com>
2020-07-30 00:20:17 -0400

    first commit

---
+++ image_resize.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageOps
+import os
+import sys
+
+path = '/Users/brianclauser/pdf to image resized'
+
+
+def newsize():
+    os.makedirs('resized', exist_ok=True)
+    for filename in os.listdir('.'):
+        if not (filename.endswith('.png') or filename.endswith('jpg')):
+            continue  # skip non-image files
+        im = Image.open(filename)
+        print('Resizing %s...' % (filename))
+        imResize = im.resize((800, 600), Image.ANTIALIAS)
+        imResize.save(os.path.join(path, filename.replace("png", "jpg")))
